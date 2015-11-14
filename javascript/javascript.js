6f3d215090f5f7d9b055bd2802a84ed43756453b
bramboomen <bram.boomen@gmail.com>
2015-11-14 01:09:05 +0000

    Collapsible items & markup

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,6 @@
-$(function() {
+/* Sticky Menu */
 
+$(function() {
 	$(window).scroll(function() {
 	    
 		var scroll_top = $(window).scrollTop();
@@ -11,4 +12,23 @@ $(function() {
 			$('menuribbon').css({ 'position': 'absolute', 'top':210}); 
 		}
 	});
+});
+
+/* Collapsible Lists */
+
+function prepareList() {
+    $('#expList').find('ul:has(blockquote)')
+    .click( function(event) {
+        if (this == event.target) {
+            $(this).toggleClass('expanded');
+            $(this).children('blockquote').slideToggle("fast");
+        }
+        return false;
+    })
+    .addClass('collapsed')
+    .children('blockquote').hide();
+}
+
+$(document).ready( function() {
+    prepareList();
 });
