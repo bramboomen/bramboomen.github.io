408e8428b2f03cb947833f4e61884d31f4a90354
bramboomen <bram.boomen@gmail.com>
2016-11-25 16:03:00 +0000

    small changes

--- javascript/javascript.1.js
+++ javascript/javascript.1.js
@@ -4,12 +4,12 @@ $(function() {
 	$(window).scroll(function() {
 	    
 		var scroll_top = $(window).scrollTop();
-		var scrolloffset = 140;
+		var scrolloffset = 133;
 		
 		if (scroll_top > scrolloffset) { 
 			$('menu').css({ 'position': 'fixed', 'top':0});
 		} else {
-			$('menu').css({ 'position': 'absolute', 'top':140}); 
+			$('menu').css({ 'position': 'absolute', 'top':133}); 
 		}
 	});
 });
