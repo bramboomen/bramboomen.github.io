797c59b2c78b70f7fa5b56627e07af980794dcd6
bramboomen <bram.boomen@gmail.com>
2016-11-25 12:31:46 +0000

    mobile ready

--- javascript/javascript.1.js
+++ javascript/javascript.1.js
@@ -4,12 +4,12 @@ $(function() {
 	$(window).scroll(function() {
 	    
 		var scroll_top = $(window).scrollTop();
-		var scrolloffset = 119;
+		var scrolloffset = 140;
 		
 		if (scroll_top > scrolloffset) { 
 			$('menu').css({ 'position': 'fixed', 'top':0});
 		} else {
-			$('menu').css({ 'position': 'absolute', 'top':119}); 
+			$('menu').css({ 'position': 'absolute', 'top':140}); 
 		}
 	});
 });
